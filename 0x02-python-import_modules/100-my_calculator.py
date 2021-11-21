1d01dc861bc1e5331a814e3d90e3c91ebdfcee8d
Timi-T <timiopebode@gmail.com>
2021-11-21 19:20:50 +0000

    import ...

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+import calculator_1
 import sys
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -12,16 +12,16 @@ if __name__ == "__main__":
     op_exist = -1
     for i in range(len(operators)):
         if op == operators[i]:
-             op_exist = i
+            op_exist = i
     if op_exist == -1:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit("1")
     if op_exist == 0:
-        c = add(a, b)
+        c = calculator_1.add(a, b)
     elif op_exist == 1:
-        c =  sub(a, b)
+        c = calculator_1.sub(a, b)
     elif op_exist == 2:
-        c = mul(a, b)
+        c = calculator_1.mul(a, b)
     else:
-        c =  div(a, b)
+        c = calculator_1.div(a, b)
     print("{:d} {:s} {:d} = {:d}".format(a, op, b, c))
