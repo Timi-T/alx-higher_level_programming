e5e15be8f2080c710535ffd9d0f5902f22f8dcc6
Timi-T <timiopebode@gmail.com>
2021-11-22 13:36:48 +0000

    if name is main

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import calculator_1
-import sys
 if __name__ == "__main__":
+    import calculator_1
+    import sys
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit("1")
