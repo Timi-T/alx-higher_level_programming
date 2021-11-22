4f0ba69d4481b40a3e05309b4da66e84045a823f
Timi-T <timiopebode@gmail.com>
2021-11-22 18:44:36 +0000

    removed dis

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    from calculator_1 import add, sub
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
@@ -8,5 +8,3 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
-import dis
-dis.dis(magic_calculation)
