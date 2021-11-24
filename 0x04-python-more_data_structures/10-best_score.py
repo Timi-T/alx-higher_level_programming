e94177a9c69b4f1a8c9448d2aabed63e4a755c72
Timi-T <timiopebode@gmail.com>
2021-11-24 20:25:19 +0000

    all equal

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,11 +3,13 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     sco = list(map(lambda x: x, (a_dictionary[key] for key in a_dictionary)))
-    for i in range(1, len(sco_list)):
+    for i in range(1, len(sco)):
         if sco[i - 1] > sco[i]:
             a = sco[i - 1]
-        else:
+        elif sco[i] > sco[i - 1]:
             a = sco[i]
+        else:
+            a = None
     for key in a_dictionary:
         if a_dictionary[key] == a:
             return key
