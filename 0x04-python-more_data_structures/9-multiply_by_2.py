011544f2a11642b6dce92c4ff60b5035e9f1cd10
Timi-T <timiopebode@gmail.com>
2021-11-24 19:47:15 +0000

    ;

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -5,5 +5,5 @@ def multiply_by_2(a_dictionary):
     new_list = list(map(lambda y: y * 2, (new_dict[key] for key in new_dict)))
     for key in new_dict:
         new_dict[key] = new_list[i]
-        i += 1;
+        i += 1
     return new_dict
