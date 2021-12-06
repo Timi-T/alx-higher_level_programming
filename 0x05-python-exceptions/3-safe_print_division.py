6fabe9171234b987ea225a0b6f1bd508066315c0
Timi-T <timiopebode@gmail.com>
2021-12-06 18:19:23 +0000

    divide

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    c = 0
+    ex = 0
+    try:
+        c = (a / b)
+    except Exception:
+        ex = 1
+    finally:
+        if ex == 1:
+            print("Inside result: {:s}".format('None'))
+            return None
+        else:
+            print("Inside result: {0:.1f}".format(c))
+            return c
