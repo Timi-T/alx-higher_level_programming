5de2d4234a9f9b1281c8e6663c1572ed263320ff
Timi-T <timiopebode@gmail.com>
2021-12-06 13:31:15 +0000

    remove ;

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    count = 0;
-    i = 0;
+    count = 0
+    i = 0
     while ((x - 1) != -1):
         try:
             print(my_list[i], end='')
