6f379616b005f225af301ef10561c016756b4e92
Timi-T <timiopebode@gmail.com>
2021-12-08 12:17:33 +0000

    documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-#python3 -c 'print(__import__("my_module").__doc__)'
-#python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
 class Square:
+    """
+    Empty square class
+    """
     pass
+    """
+    Function that does nothing
+    """
