17610cd8221f4939b98d33025a1dfdbff102025a
Timi-T <timiopebode@gmail.com>
2021-12-08 14:58:06 +0000

    area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
+class Square:
+    """
+    class to represent a square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializing element
+        """
+        try:
+            if size < 0:
+                raise ValueError
+            else:
+                self.__size = size
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
