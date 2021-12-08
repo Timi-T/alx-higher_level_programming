48b7c44f0217d9a016d46e3aa14dfba2edb75745
Timi-T <timiopebode@gmail.com>
2021-12-08 12:26:22 +0000

    pep8

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
 class Square:
+    """
+    A class to rep a square
+    """
+
     def __init__(self, size):
+        """
+        Initialization of a square
+        """
         self.__size = size
