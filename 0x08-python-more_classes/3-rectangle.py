b1ed54d39137e19183b87b41c2d141f8ec5b8aee
Timi-T <timiopebode@gmail.com>
2021-12-13 17:16:52 +0000

    0-3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/bin:Wq/python3
+"""
+module for defining a rectangle
+"""
+
+
+class Rectangle:
+    """
+    rectangle class
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        initialization of instance
+        """
+        if type(width) != type(1):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) != type(1):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height musts be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        getter for width
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        getter for height
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """
+        setter for width
+        """
+        if type(value) != type(1):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """
+        setter for height
+        """
+        if type(value) != type(1):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        function to return area
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """
+        function to return rectangle perimeter
+        """
+        return (self.__width * 2) + (self.__height *2)
+
+    def __str__(self):
+        """
+        function to represent a squre with #
+        """
+        for h in range(self.__height):
+            for w in range(self.__width):
+                print('#', end='')
+            if h != self.__height - 1:
+                print()
+        return ''
