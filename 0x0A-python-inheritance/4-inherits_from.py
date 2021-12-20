b24efb581f8002631ea7da5b4d3838eaaad017e5
Timi-T <timiopebode@gmail.com>
2021-12-20 18:32:57 +0000

    4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module with function to chech if object inherits from class or superclass
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    function to check if object inherits from class or superclass
+    """
+    if issubclass(a_class, type(obj)):
+        return False
+    else:
+        return True
