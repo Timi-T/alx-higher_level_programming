77ae6c6f9b0238d2716ba5e80fd569e63c7c3b14
Timi-T <timiopebode@gmail.com>
2021-12-20 18:30:17 +0000

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module with function to check for class or subclass
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    function to check for class or subclass
+    """
+    return isinstance(obj, a_class)
