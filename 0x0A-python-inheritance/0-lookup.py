4a5289d6b32ed9bcca891e7e914cf3336e6f2bdb
Timi-T <timiopebode@gmail.com>
2021-12-20 13:01:03 +0000

    documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+module to print out attributes and methods of a class
+"""
 def lookup(obj):
+    """
+    function to print out attributes and methods of a class
+    """
     return dir(obj)
