09b9ef6657f03887bd740481f6ce1afaf2e02525
Timi-T <timiopebode@gmail.com>
2021-12-21 18:46:54 +0000

    add not overwrite

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,8 +9,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-with open('add_item.json', mode='w', encoding='utf-8') as a_file:
-    a_file.write('[]')
+with open('add_item.json', mode='r+', encoding='utf-8') as a_file:
+    if a_file.read() == '':
+        a_file.write('[]')
 list_content = load_from_json_file('add_item.json')
 argc = len(sys.argv)
 for i in range(1, argc):
