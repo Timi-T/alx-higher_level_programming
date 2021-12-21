3e0c595009f404eac14ab975f67314539314cf96
Timi-T <timiopebode@gmail.com>
2021-12-21 18:31:59 +0000

    7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+python script to add arguments to a python list
+"""
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+list_content = load_from_json_file('add_item.json')
+argc = len(sys.argv)
+for i in range(1, argc):
+    list_content.append(sys.argv[i])
+save_to_json_file(list_content, 'add_item.json')
