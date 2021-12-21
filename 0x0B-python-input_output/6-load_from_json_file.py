e608c5bf5a7889a045f20fd8a577763ebd642438
Timi-T <timiopebode@gmail.com>
2021-12-21 14:56:14 +0000

    6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module with function to decode json to object
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    function to decode json to object
+    """
+    with open(filename, mode='r', encoding='utf-8') as a_file:
+        json.load(a_file)
