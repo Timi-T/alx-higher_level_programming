6c49b90a7d21f10f97645b2ebf63560e370747c4
Timi-T <timiopebode@gmail.com>
2021-12-21 19:11:45 +0000

    all conditions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,12 +5,16 @@ python script to add arguments to a python list
 
 
 import sys
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-with open('add_item.json', mode='r+', encoding='utf-8') as a_file:
-    if a_file.read() == '':
+if os.path.exists('add_item.json') and os.stat('add_item.json').st_size == 0:
+    with open('add_item.json', mode='w', encoding='utf-8') as a_file:
+        a_file.write('[]')
+elif os.path.exists('add_item.json') is False:
+    with open('add_item.json', mode='w', encoding='utf-8') as a_file:
         a_file.write('[]')
 list_content = load_from_json_file('add_item.json')
 argc = len(sys.argv)
