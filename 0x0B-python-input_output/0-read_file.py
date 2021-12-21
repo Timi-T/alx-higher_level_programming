185bc1fa60f4c3af89f9c444dd5ba2d8af1a2a98
Timi-T <timiopebode@gmail.com>
2021-12-21 13:02:36 +0000

    0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module with function to read files
+"""
+
+
+def read_file(filename=""):
+    """
+    function to read a file
+    """
+    with open(filename, encoding='utf-8') as a_file:
+        file_content = a_file.read()
+    print(file_content)
