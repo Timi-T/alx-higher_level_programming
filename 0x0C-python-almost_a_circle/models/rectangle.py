b263265de7b44050f7a6d4da9a7b4812083a0484
Timi-T <timiopebode@gmail.com>
2022-01-03 07:12:40 +0000

    pycode and documentation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,7 +23,10 @@ class Rectangle(Base):
         super().__init__(id)
 
     def validate_dim(self, dim_value, dim_name=""):
-        if (isinstance(dim_value, int) is False) or (isinstance(dim_value, bool) is True):
+        """method to validate dimensions (width and height)
+        """
+        if (isinstance(dim_value, int) is False) or\
+                (isinstance(dim_value, bool) is True):
             raise TypeError("{} must be an integer".format(dim_name))
         elif dim_value <= 0:
             raise ValueError("{} must be > 0".format(dim_name))
@@ -31,7 +34,10 @@ class Rectangle(Base):
             return True
 
     def validate_pos(self, pos_value, pos_axis=''):
-        if isinstance(pos_value, int) is False or (isinstance(pos_value, bool) is True):
+        """method to validate position (x and y)
+        """
+        if (isinstance(pos_value, int) is False) or\
+                (isinstance(pos_value, bool) is True):
             raise TypeError("{} must be an integer".format(pos_axis))
         elif pos_value < 0:
             raise ValueError("{} must be >= 0".format(pos_axis))
@@ -41,44 +47,64 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter for width
+        """
         return self.__width
 
     @property
     def height(self):
+        """getter for height
+        """
         return self.__height
 
     @property
     def x(self):
+        """getter for x
+        """
         return self.__x
 
     @property
     def y(self):
+        """getter for y
+        """
         return self.__y
 
     @width.setter
     def width(self, value):
+        """setter for width
+        """
         self.validate_dim(value, "width")
         self.__width = value
 
     @height.setter
     def height(self, value):
+        """setter for height
+        """
         self.validate_dim(value, "height")
         self.__height = value
 
     @x.setter
     def x(self, value):
+        """setter for x
+        """
         self.validate_pos(value, 'x')
         self.__x = value
 
     @y.setter
     def y(self, value):
+        """setter for y
+        """
         self.validate_pos(value, 'y')
         self.__y = value
 
     def area(self):
+        """method to return area of rectangle
+        """
         return self.__width * self.__height
 
     def display(self):
+        """method to display rectangleon the screen
+        """
         for a in range(self.__y):
             print()
         for i in range(self.__height):
@@ -89,10 +115,19 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
-        return "[{}] ({:d}) {:d}/{:d} - {:d}/{:d}".format(__class__.__name__,
-                self.id, self.__x, self.__y, self.__width, self.__height)
+        """method to print object on screen
+        """
+        form = "[{}] ({:d}) {:d}/{:d} - {:d}/{:d}"
+        name = __class__.__name__
+        s_x = self.__x
+        s_y = self.__y
+        s_w = self.__width
+        s_h = self.__height
+        return form.format(name, self.id, s_x, s_y, s_w, s_h)
 
     def update(self, *args, **kwargs):
+        """method to update an instance using args or kwargs
+        """
         try:
             self.id = args[0]
             self.validate_dim(args[1], "width")
@@ -123,6 +158,8 @@ class Rectangle(Base):
                     self.__y = value
 
     def to_dictionary(self):
+        """method to get the dictionary representation of an instance
+        """
         dic_rep = {}
         dic_rep["id"] = self.id
         dic_rep["width"] = self.width
