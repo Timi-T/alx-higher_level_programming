b2ff238b3ad7badf4cf6bf913cfb80eb5fd95acd
Timi-T <timiopebode@gmail.com>
2022-01-03 07:31:54 +0000

    create

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -60,7 +60,10 @@ class Base:
     def create(cls, **dictionary):
         """method to create an instance of this class
         """
-        r1 = cls(10, 10)
+        if cls.__name__ == "Rectangle":
+            r1 = cls(10, 10, 10, 10)
+        elif cls.__name__ == "Square":
+            r1 = cls(10, 10, 10)
         instances = []
         r1.update(**dictionary)
         return r1
