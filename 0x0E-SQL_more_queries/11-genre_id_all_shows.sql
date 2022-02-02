da505e547536903f870513db2f5581e3f95c9a52
Timi-T <timiopebode@gmail.com>
2022-02-02 13:51:49 -0800

    left join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- List all shows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
 ORDER BY title, genre_id;
