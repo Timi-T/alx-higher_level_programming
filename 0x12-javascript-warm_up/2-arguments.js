b2ecdea84433e64ee0d16f6c695237b78e4001d6
Timi-T <timiopebode@gmail.com>
2022-02-28 14:16:40 +0000

    semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 let myOutput;
-myArgs = process.argv.slice(2);
+const myArgs = process.argv.slice(2);
 if (myArgs.length === 0) {
   myOutput = 'No argument';
 } else if (myArgs.length === 1) {
