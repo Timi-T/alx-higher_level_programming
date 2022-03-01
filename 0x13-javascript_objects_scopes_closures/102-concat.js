a229a4d0be9b0c8e19f027c63b804c2708f1276e
Timi-T <timiopebode@gmail.com>
2022-03-01 19:49:19 +0000

    args

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,14 +2,15 @@
 
 const fs = require('fs');
 
+const myArgs = process.argv.slice(2);
 let textOne;
-fs.readFile('fileA', 'utf-8', (err, data) => {
+fs.readFile(myArgs[0], 'utf-8', (err, data) => {
   if (err) throw err;
   textOne = data;
-  fs.readFile('fileB', 'utf-8', (err, data) => {
+  fs.readFile(myArgs[1], 'utf-8', (err, data) => {
     if (err) throw err;
     const textThree = textOne + data;
-    fs.writeFile('fileC', textThree, (err) => {
+    fs.writeFile(myArgs[2], textThree, (err) => {
       if (err) throw err;
     });
   });
