ccd849680cd45b568879aad9c2721d1c206b1198
Timi-T <timiopebode@gmail.com>
2022-03-01 17:42:16 +0000

    object magic

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 
+const increment = {}
+increment.index = 0;
 const oldArray = require('./100-data').list;
-const newArray = oldArray.map(x => x * oldArray.indexOf(x));
+const newArray = oldArray.map(myFunction);
+function myFunction (x) {
+  return x * increment.index++;
+}
 console.log(oldArray);
 console.log(newArray);
