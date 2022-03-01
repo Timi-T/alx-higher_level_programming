0910b834b332cdaf144df7585db502c3703e572f
Timi-T <timiopebode@gmail.com>
2022-03-01 11:18:32 +0000

    8 correction

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let strLen = list.length - 1;
-  let newList = [];
+  const strLen = list.length - 1;
+  const newList = [];
   for (let i = strLen, j = 0; i >= 0; i--, j++) {
     newList[j] = list[i];
   }
