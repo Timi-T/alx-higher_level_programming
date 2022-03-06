85ccbcaf394d54aab529acc40221a3759845a45d
Timi-T <timiopebode@gmail.com>
2022-03-06 04:44:30 -0800

    if name is main

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-
-
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-
-
-user = sys.argv[1]
-pwd = sys.argv[2]
-db_name = sys.argv[3]
-state_name = sys.argv[4]
+"""
+module to get state id of a state
+"""
 
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
     eng_txt = 'mysql+mysqldb://{}:{}@localhost/{}'.format(user, pwd, db_name)
     engine = create_engine(eng_txt, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
