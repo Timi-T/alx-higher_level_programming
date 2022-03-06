8b253a06b808f13f05ffd2e486b641da63db0d27
Timi-T <timiopebode@gmail.com>
2022-03-06 03:11:32 -0800

    fiter in printout not in query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,9 +15,10 @@ if __name__ == "__main__":
     mydb = MySQLdb.connect(host="localhost", port=3306,
                            user=username, passwd=password, db=db_name)
     db_cursor = mydb.cursor()
-    q = """SELECT * FROM states WHERE name REGEXP '^N' ORDER BY states.id"""
+    q = """SELECT * FROM states ORDER BY states.id"""
     db_cursor.execute(q)
     table = db_cursor.fetchall()
 
     for row in table:
-        print(row)
+        if row[1].startswith("N"):
+            print(row)
