387ed161ac050ffca41fc00dcf4b81a341a928e4
Timi-T <timiopebode@gmail.com>
2022-03-06 04:09:10 -0800

    if name is ain

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,17 +3,17 @@
 module to get all states containing letter a
 """
 
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-
-
-user = sys.argv[1]
-pwd = sys.argv[2]
-db_name = sys.argv[3]
-
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+
     eng_txt = 'mysql+mysqldb://{}:{}@localhost/{}'.format(user, pwd, db_name)
     engine = create_engine(eng_txt, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
@@ -21,4 +21,4 @@ if __name__ == "__main__":
 
     A_states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
     for state in A_states:
-        print(state.id, ': ' + state.name)
+        print("{}: {}".format(state.id, state.name))
