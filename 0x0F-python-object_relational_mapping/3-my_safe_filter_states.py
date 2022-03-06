e063efe8a3bd1ea6d3cff967a1c8c8bd45a243ca
Timi-T <timiopebode@gmail.com>
2022-03-06 03:32:39 -0800

    safe query

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,6 +2,7 @@
 """
 module to prevent sql injection
 """
+
 if __name__ == "__main__":
     import MySQLdb
     import sys
@@ -10,13 +11,12 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
-    state_name = "'" + state_name + "'"
 
     mydb = MySQLdb.connect(user=username, passwd=password, db=db_name)
     db_cursor = mydb.cursor()
-    txt1 = "SELECT * FROM states WHERE "
-    txt2 = "name = {} ORDER BY states.id".format(state_name)
-    db_cursor.execute(txt1 + txt2)
+    db_cursor.execute(
+            "SELECT * FROM states WHERE name = %s ORDER BY states.id",
+            (state_name, ))
     states = db_cursor.fetchall()
 
     for each_state in states:
