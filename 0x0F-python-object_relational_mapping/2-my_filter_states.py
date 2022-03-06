cafbb175c8d89aec737df4ee849ac14ea623ccba
Timi-T <timiopebode@gmail.com>
2022-03-05 23:00:41 -0800

    name in quote

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,12 +11,12 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
-    """ state_name = "'" + state_name + "'" """
+    state_name = "'" + state_name + "'"
 
     mydb = MySQLdb.connect(user=username, passwd=password, db=db_name)
     db_cursor = mydb.cursor()
-    db_cursor.execute("""SET @state_name = {}""".format(state_name))
-    db_cursor.execute("""SELECT * FROM states WHERE name = @state_name""")
+    q = "SELECT * FROM states WHERE states.name = {}".format(state_name)
+    db_cursor.execute(q)
     states = db_cursor.fetchall()
 
     for each_state in states:
