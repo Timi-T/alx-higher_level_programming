c0ab81a950b8871ed5acda05ddf06abdb910321c
Timi-T <timiopebode@gmail.com>
2022-03-18 05:42:40 -0700

    if name is main

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,12 +3,12 @@
 send a request to a url
 """
 
-import sys
-from urllib.request import urlopen
-import json
+if __name__ == "__main__":
+    import sys
+    from urllib.request import urlopen
+    import json
 
-
-url = sys.argv[1]
-with urlopen(url) as response:
-    pass
-print(response.headers['X-Request-Id'])
+    url = sys.argv[1]
+    with urlopen(url) as response:
+        pass
+    print(response.headers['X-Request-Id'])
