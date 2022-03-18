7b01e528ed7daa50c22dc89c6874228c276f9bbc
Timi-T <timiopebode@gmail.com>
2022-03-18 05:12:48 -0700

    if name is main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,11 @@
 send a request to a url
 """
 
-from urllib.request import urlopen
-import json
+if __name__ == "__main__":
+    from urllib.request import urlopen
+    import json
 
-
-with urlopen("https://alx-intranet.hbtn.io/status") as response:
-    body = response.read()
-print('Body response:\n    - type: {}\n    - content: {}\n    - utf8 content:\
-      {}'.format(type(body), body, body.decode('utf-8')))
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+    print('Body response:\n    - type: {}\n    - content: {}\n    - utf8 \
+content: {}'.format(type(body), body, body.decode('utf-8')))
