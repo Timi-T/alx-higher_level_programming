4ac275737ff51d91887fb9638e6fb3e34ecc549b
Timi-T <timiopebode@gmail.com>
2022-03-20 01:06:16 -0700

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,14 +2,15 @@
 """
 send a request to a url
 """
+if __name__ == "__main__":
+    import sys
+    from urllib import parse, request
+    import json
 
-import sys
-from urllib import parse, request
-import json
-
-
-url = sys.argv[1]
-data = 
-with urlopen(url) as response:
-    pass
-print(response.headers['X-Request-Id'])
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+    data = parse.urlencode(data).encode('ascii')
+    with request.urlopen(url, data) as c:
+        content = c.read().decode('utf-8')
+    print(content)
