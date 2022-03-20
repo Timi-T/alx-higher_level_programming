579f36448f1c8bc56502d37954d681e4aafbae0f
Timi-T <timiopebode@gmail.com>
2022-03-20 02:49:24 -0700

    task 4 requests module

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,9 +5,9 @@ script to get status of a response
 
 if __name__ == "__main__":
 
-    from urllib.request import urlopen
+    import requests
 
     url = 'https://alx-intranet.hbtn.io/status'
-    with urlopen(url) as response:
-        b = response.read().decode('utf-8')
+    response = requests.get(url)
+    b = response.text
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(b), b))
