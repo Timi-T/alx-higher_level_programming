c0df6e073c276b4a809751698efcaf08aa5193de
Timi-T <timiopebode@gmail.com>
2022-03-20 02:55:00 -0700

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+use requests module to get header value
+"""
+
+if __name__ == "__main__":
+
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers['X-Request-Id'])
